70b650c51ccc19bf914fab828c4f559a8d215560
LuckyCode7 <nowak9.private@gmail.com>
2018-10-10 21:51:09 +0200

    BoardTests added

---
+++ test/BoardTests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "Board.hpp"
+
+struct BoardTest : public ::testing::Test
+{
+    Board board;
+};
+
+TEST_F(BoardTest, check_if_board_contains_40_fields)
+{
+    ASSERT_EQ(40, board.getNumberOfFields());
+}
