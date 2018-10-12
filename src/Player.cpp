c1d491d1f8efa5d0842c84ed3906f9befc8f3e16
Anna Bernaś <40730866+anmaber@users.noreply.github.com>
2018-10-13 01:08:09 +0200

    [US.2][1] Object representation of the player #7  (#11)
    * added new files
    
    * added new files
    
    * created Player class and tests
    
    * location setter added
    
    * added adding Property
    
    * changed moving constructor label to delete
    
    * changed color_ from string to enum and created alias for std::vector<Property>
    
    * changed ownsProperty to hasAnyProperty
    
    * added ownsProperty function
    
    * changed Tests names
    
    * changed vector of properties into vector<std::shared_ptr<Property>>
    
    * Added blank lines at the end of the files
    
    * Added blank lines at the end of the files
    
    * Update Player.cpp
    
    * Removed hasAnyProperty and ownsProperty
    
    * Removed tests for hasAnyProperty and ownsProperty
    
    * Update Player.cpp
    
    * Added const & to shared_ptr<Property> in addProperty

---
+++ src/Player.cpp
@@ -0,0 +1,33 @@
+#include "Player.hpp"
+#include <algorithm>
+
+Player::Player(Color c)
+        : color_(c),
+        money_(3000),
+        location_(0)
+{}
+
+Color Player::getColor() const
+{
+    return color_;
+}
+
+int Player::amountOfMoney() const
+{
+    return money_;
+}
+
+int Player::getLocation() const
+{
+    return location_;
+}
+
+void Player::setLocation(const int location)
+{
+    location_ = location;
+}
+
+void Player::addProperty(const std::shared_ptr<Property> & property)
+{
+    properties_.push_back(property);
+}
